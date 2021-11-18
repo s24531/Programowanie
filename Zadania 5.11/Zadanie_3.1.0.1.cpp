2b1cad9dfa210bb077b2921cb8cfb9f0d1500896
s24531 <s24531@pjwstk.edu.pl>
2021-11-18 18:02:41 +0100

    Zadanie

---
+++ Zadania 5.11/Zadanie_3.1.0.1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+auto init (int a[], int n)
+{
+    a[n];
+    for(auto i=0; i<n; i++)
+    {
+       a[i] = 0; 
+    }
+}
+
+auto main() -> int
+{
+    int a[100];
+    int n;
+    
+    std::cout << "Ilość zer w tablicy: ";
+    std::cin >> n;
+    
+    init(a, n);
+    
+    for(auto i=0; i<n; i++)
+    {
+        std::cout << "a[" << i << "] = " << a[i] << "\n";
+    }
+   
+    return 0;
+}
