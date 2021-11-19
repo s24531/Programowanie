df81b42efe433471295abf714ff4c2d9858d335d
Student <s24531@pjwstk.edu.pl>
2021-11-19 13:19:53 +0100

    Zadanie

--- Zadania 5.11/Zadanie_2.0.0.12.cpp
+++ Zadania 5.11/Zadanie_2.0.0.12.cpp
@@ -5,15 +5,9 @@ auto main (int argc, char* argv []) -> int
 {
 	auto const a = std::stoi(argv[1]);
 
-<<<<<<< HEAD
-    for(auto i=a; i>=i;i--)
-    {
-        for(auto j=i; j>=a; j--)
-=======
     for(auto i=a; i>=1;i--)
     {
         for(auto j=i; j<=a; j++)
->>>>>>> 4b1003e0f5fb79080c7b803d9c7e7a04f702a337
         {
             std::cout << "*";
         }   
