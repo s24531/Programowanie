744f9ef710f4cd5ed8024eb720316eefa5414ba0
Student <s24531@pjwstk.edu.pl>
2021-11-19 13:21:58 +0100

    Zadanie

--- Zadania 5.11/Zadanie_2.0.0.5.cpp
+++ Zadania 5.11/Zadanie_2.0.0.5.cpp
@@ -1,45 +1,5 @@
 #include <iostream>
 
-<<<<<<< HEAD
-auto main() -> int
-{
-    int suma=0, liczba, zlicz=0;
-
-    std::cout << "Podaj liczbę: ";
-    std::cin >> liczba;
-
-    for(auto i=liczba; i>=2; i--)
-    {
-        std::cout << i << "p\n";
-        if(liczba%i==0)
-            {
-                zlicz++;
-                for(auto x=liczba; x>=2; x--)
-                {
-                    std::cout<< i << "\n";
-                    if(x%i==0)
-                    {
-                        suma=suma+i;
-                        std::cout << i << "\n";
-                    }       
-                }
-            }
-    }
-
-    if(zlicz>2)
-    {
-        std::cout << "Liczba " << liczba << " nie jest liczbą pierwszą\n";
-    }
-    else
-    {
-        std::cout << "Liczba " << liczba << " jest liczbą pierwszą\n";
-        std::cout << "Suma liczb pierwszych wynosi: " << suma << "\n";
-    }
-    
-    return 0;
-
-}
-=======
 bool czy_pierwsza (int n)
 {
     int licz=0;
@@ -88,4 +48,3 @@ auto main() -> int
     }
     return 0;
 }
->>>>>>> 4b1003e0f5fb79080c7b803d9c7e7a04f702a337
