da1d3275511453b73a9a4ea4ec3894383af2636b
s24531 <s24531@pjwstk.edu.pl>
2021-12-30 19:22:25 +0100

    Projekt

--- Projekt/Kalkulator.cpp
+++ Projekt/Kalkulator.cpp
@@ -1,189 +1,204 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string>
 
-auto exit1 () -> void
+auto error () -> void
 {
     std::cout << "ERR\n";
 }
 
+auto check_number (std::string num) -> bool
+{
+    for (auto i = 0; i < num.length(); i++)
+    {
+        if (isdigit(num[i]))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }    
+    }
+}
+
+auto enter_matrix (int row1, int col2)
+{
+    int * matrix = new int [row1 * col2];
+    std::string num;
+    int num1;
+
+    //wprowadzanie liczb do macierzy
+    for(auto i = 0; i < row1; i++)
+    {
+        for(auto j = 0; j < col2; j++)
+        {
+            std::cout << "A[" << i+1 << "][" << j+1 << "]: ";
+            std::cin >> num;
+            if(check_number(num))
+            {   
+                num1 = std::stoi(num);
+                matrix[i*col2+j] = num1;
+            }
+            else
+            {
+                std::cout << "To nie jest liczba\n";
+                j--;
+            }
+        }
+    }
+    return matrix;
+}
+
+auto display_matrix(int * matrix, int row1, int col2) -> void
+{
+    for(auto i = 0; i < row1; i++)
+    {
+        for(auto j = 0; j < col2; j++)
+        {
+            std::cout << matrix[i*row1+j];
+            if(j==col2-1)
+            {
+                std::cout << "\n";
+            }
+            else
+            {
+                std::cout << " ";
+            }
+        }
+    }
+}
+
 auto main() -> int
 {
-    int kalk;
+    int calc;
 
-    std::cout << "Kalkulator macierzowy\nProszę wybrać odpowiednią opcję:\n";
+    std::cout << "Kalkulator macierzowy\n";
+    std::cout << "Proszę wybrać odpowiednią opcję:\n";
     std::cout << "1. Dodawanie i odejmowanie macierzy.\n";
     std::cout << "2. Mnożenie macierzy.\n";
     std::cout << "3. Transpozycja macierzy.\n";
     std::cout << "4. Obliczanie wyznacznika macierzy kwadratowej.\n";
     std::cout << "Opcja: ";
-    std::cin >> kalk;
+    std::cin >> calc;
 
-    switch(kalk)
+    switch(calc)
     {
         case 1:
         {
-            int wym1, wym2;
+            int row1, col1;
             
             std::cout << "Podaj ilosc wierszy pierwszej macierzy: "; 
-            std::cin >> wym1;
+            std::cin >> row1;
             std::cout << "Podaj ilość kolumn pierwszej macierzy: ";
-            std::cin >> wym2;
+            std::cin >> col1;
 
-            int* macierzA = new int [wym1 * wym2];
+            std::cout << "Wprowadź elementy do pierwszej macierzy: \n";
+            int * matrixA = enter_matrix(row1, col1);
 
 
-            int wym3, wym4;
+            int row2, col2;
 
             std::cout << "Podaj ilosc wierszy drugiej macierzy: "; 
-            std::cin >> wym3;
+            std::cin >> row2;
             std::cout << "Podaj ilość kolumn drugiej macierzy: ";
-            std::cin >> wym4;
-
-            int* macierzB = new int [wym3 * wym4];
+            std::cin >> col2;
             
+            std::cout << "Wprowadź elementy do drugiej macierzy: \n";
+            int * matrixB = enter_matrix(row2, col2);
 
-            int* dodaj =  new int [wym1 * wym2];
-            int* odejmij = new int [wym1 * wym2];
             
             //jeśli macierze mają inne rozmiary to zakończ program
-            if(wym1!=wym3 || wym2!=wym4)
+            if(row1!=row2 || col2!=col2)
             {   
-                atexit(exit1);
+                atexit(error);
                 exit(0);
-            }
+            }       
 
-            //wprowadzanie liczb do pierwszej macierzy
-            std::cout << "Wprowadź elementy w pierwszej macierzy: \n";
-            for(auto i = 0; i < wym1; i++)
-            {
-                for(auto j = 0; j < wym2; j++)
-                {
-                    std::cout << "A[" << i+1 << "][" << j+1 << "]: ";
-                    std::cin >> macierzA[i*wym2+j];
-                }
-            }
 
-            //wprowadzanie liczb do drugiej macierzy
-            std::cout << "Wprowadź elementy w drugiej macierzy: \n";
-            for(auto i = 0; i < wym3; i++)
-            {
-                for(auto j = 0; j < wym4; j++)
-                {   
-                    std::cout << "B[" << i+1 << "][" << j+1 << "]: ";
-                    std::cin >> macierzB[i*wym4+j];
-                }
-            }
+            int* add =  new int [row1 * col1];
+            int* sub = new int [row1 * col1];
+
 
             //dodawanie i odejmowanie macierz
-            for(auto i = 0; i < wym1; i++)
+            for(auto i = 0; i < row1; i++)
             {
-                for(auto j = 0; j < wym2; j++)
+                for(auto j = 0; j < col1; j++)
                 {
-                    dodaj[i*wym1+j] = macierzA[i*wym1+j] + macierzB[i*wym3+j];
-                    odejmij[i*wym1+j] = macierzA[i*wym1+j] - macierzB[i*wym3+j];
+                    add[i*row1+j] = matrixA[i*row1+j] + matrixB[i*col1+j];
+                    sub[i*row1+j] = matrixA[i*row1+j] - matrixB[i*col1+j];
                 }
             }
+
 
             //wyświetlenie wyniku dodawania
-            std::cout << "Wynik dodawania: \n";
-            for(auto i = 0; i < wym1; i++)
-            {
-                for(auto j = 0; j < wym2; j++)
-                {
-                    std::cout << dodaj[i*wym1+j];
-                    if(j==wym2-1)
-                    {
-                        std::cout << "\n";
-                    }
-                    else
-                    {
-                        std::cout << " ";
-                    }
-                }
-            }
+            std::cout << "Wynik dodawania:\n";
+            display_matrix(add, row1, col2);
 
-            //wyświetlenie wyniku odejmowania
-            std::cout << "Wynik odejmowania: \n";
-            for(auto i = 0; i < wym1; i++)
-            {
-                for(auto j = 0; j < wym2; j++)
-                {
-                    std::cout << odejmij[i*wym1+j];
-                    if(j==wym2-1)
-                    {
-                        std::cout << "\n";
-                    }
-                    else
-                    {
-                        std::cout << " ";
-                    }
-                }
-            }
+
+            //wyświetlenie resultu odejmowania
+            std::cout << "Wynik odejmowania:\n";
+            display_matrix(sub, row1, col2);
+
 
             break;
         }
         case 2:
         {
-            int wym1, wym2;
+            int row1, col1;
             
-            std::cout << "Podaj ilosc wierszy pierwszej macierzy: "; 
-            std::cin >> wym1;
+            std::cout << "Podaj ilość wierszy pierwszej macierzy: "; 
+            std::cin >> row1;
             std::cout << "Podaj ilość kolumn pierwszej macierzy: ";
-            std::cin >> wym2;
+            std::cin >> col1;
 
-            int* macierzA = new int [wym1 * wym2];
+            std::cout << "Wprowadź elementy do pierwszej macierzy: \n";
+            int * matrixA = enter_matrix(row1, col1);
 
 
-            int wym3, wym4;
+            int row2, col2;
 
-            std::cout << "Podaj ilosc wierszy drugiej macierzy: "; 
-            std::cin >> wym3;
+            std::cout << "Podaj ilość wierszy drugiej macierzy: "; 
+            std::cin >> row2;
             std::cout << "Podaj ilość kolumn drugiej macierzy: ";
-            std::cin >> wym4;
+            std::cin >> col2;
 
-            int* macierzB = new int [wym3 * wym4];
+            std::cout << "Wprowadź elementy do drugiej macierzy: \n";
+            int * matrixB = enter_matrix(row2, col2);
             
+
             //jeśli liczba kolumn w macierzy A i liczba wierszy w macierzy B są różne to zakończ program
-            if(wym2!=wym3)
+            if(col1!=row2)
             {   
-                atexit(exit1);
+                atexit(error);
                 exit(0);
             }
 
 
-            break;
-        }
-        case 3:
-        {   
-            int wym1, wym2;
+            int* result =  new int [col1 * row2];
+            int temp;
             
-            std::cout << "Podaj ilosc wierszy macierzy: "; 
-            std::cin >> wym1;
-            std::cout << "Podaj ilość kolumn macierzy: ";
-            std::cin >> wym2;
-        
-            int* macierzA = new int [wym1 * wym2];
-            
-            //wprowadzanie liczb do macierzy
-            std::cout << "Wprowadź elementy w pierwszej macierzy: \n";
-            for(auto i = 0; i < wym1; i++)
+            for(auto i = 0; i < row1; i++)
             {
-                for(auto j = 0; j < wym2; j++)
+                for(auto j = 0; j < col2; j++)
                 {
-                    std::cout << "A[" << i+1 << "][" << j+1 << "]: ";
-                    std::cin >> macierzA[i*wym2+j];
+                    temp = 0;
+                    for(auto k = 0; k < row1; k++)
+                    {
+                        temp += matrixA[i*col1+k] * matrixB[k*col2+j];
+                    }                 
+                    result[i*col2+j] = temp;
                 }
             }
-            
 
-            //wyświetlenie podanej przez uzytkownika macierzy
-            std::cout << "Macierz podana przez użytkownika: \n";
-            for(auto i = 0; i < wym1; i++)
+
+            std::cout << "Wynik mnożenia: \n";
+            for(auto i = 0; i < row1; i++)
             {
-                for(auto j = 0; j < wym2; j++)
+                for(auto j = 0; j < col2; j++)
                 {
-                    std::cout << macierzA[i*wym1+j];
-                    if(j==wym2-1)
+                    std::cout << result[i*col2+j];
+                    if(j==col2-1)
                     {
                         std::cout << "\n";
                     }
@@ -194,18 +209,36 @@ auto main() -> int
                 }
             }
 
+            break;
+        }
+        case 3:
+        {   
+            int row1, col1;
+            
+            std::cout << "Podaj ilość wierszy macierzy: "; 
+            std::cin >> row1;
+            std::cout << "Podaj ilość kolumn macierzy: ";
+            std::cin >> col1;
 
-            std::swap(wym1,wym2);
+            std::cout << "Wprowadź elementy do macierzy: \n";
+            int* matrixA = enter_matrix(row1, col1);
+            
+
+            //wyświetlenie podanej przez uzytkownika macierzy
+            display_matrix(matrixA, row1, col1);
+
+
+            std::swap(row1,col1);
 
 
             //wyświetlenie transponowanej macierzy
             std::cout << "Macierz transponowana: \n";
-            for(auto i = 0; i < wym1; i++)
+            for(auto i = 0; i < row1; i++)
             {
-                for(auto j = 0; j < wym2; j++)
+                for(auto j = 0; j < col1; j++)
                 {
-                    std::cout << macierzA[j*wym1+i];
-                    if(j==wym2-1)
+                    std::cout << matrixA[j*row1+i];
+                    if(j==col1-1)
                     {
                         std::cout << "\n";
                     }
@@ -221,6 +254,26 @@ auto main() -> int
         }
         case 4:
         {
+            int row1, col1;
+            
+            std::cout << "Podaj ilość wierszy macierzy: "; 
+            std::cin >> row1;
+            std::cout << "Podaj ilość kolumn macierzy: ";
+            std::cin >> col1;
+
+
+            //jeśli liczba kolumn i wierszy nie są sobie równe to zakończ program
+            if(row1!=col1)
+            {   
+                atexit(error);
+                exit(0);
+            }
+
+
+            std::cout << "Wprowadź elementy do macierzy: \n";
+            int* matrixA = enter_matrix(row1, col1);
+
+            
 
 
 
