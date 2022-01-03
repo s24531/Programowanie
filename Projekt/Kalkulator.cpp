cacf21eabc4a6700662e68cbd42d1ba79ad65219
s24531 <s24531@pjwstk.edu.pl>
2022-01-03 01:18:05 +0100

    Projekt

--- Projekt/Kalkulator.cpp
+++ Projekt/Kalkulator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <cmath>  
 
 auto error () -> void
 {
@@ -11,34 +12,48 @@ auto check_number (std::string num) -> bool
 {
     for (auto i = 0; i < num.length(); i++)
     {
-        if (isdigit(num[i]))
+        if(num[0]=='-')
         {
-            return true;
+            if(isdigit(num[i+1]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
-            return false;
-        }    
+            if(isdigit(num[i]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }   
+        } 
     }
 }
 
-auto enter_matrix (int row1, int col2)
+auto enter_matrix (int row1, int col1)
 {
-    int * matrix = new int [row1 * col2];
+    int matrix[20][20];
     std::string num;
     int num1;
 
     //wprowadzanie liczb do macierzy
     for(auto i = 0; i < row1; i++)
     {
-        for(auto j = 0; j < col2; j++)
+        for(auto j = 0; j < col1; j++)
         {
             std::cout << "A[" << i+1 << "][" << j+1 << "]: ";
             std::cin >> num;
             if(check_number(num))
             {   
                 num1 = std::stoi(num);
-                matrix[i*col2+j] = num1;
+                matrix[i][j] = num1;
             }
             else
             {
@@ -50,14 +65,15 @@ auto enter_matrix (int row1, int col2)
     return matrix;
 }
 
-auto display_matrix(int * matrix, int row1, int col2) -> void
+auto display_matrix(int matrix[20][20], int row1, int col1) -> void
 {
+    std::cout << "\nMacierz podana przez użytkownika:\n";
     for(auto i = 0; i < row1; i++)
     {
-        for(auto j = 0; j < col2; j++)
+        for(auto j = 0; j < col1; j++)
         {
-            std::cout << matrix[i*row1+j];
-            if(j==col2-1)
+            std::cout << matrix[i][j];
+            if(j==col1-1)
             {
                 std::cout << "\n";
             }
@@ -69,9 +85,44 @@ auto display_matrix(int * matrix, int row1, int col2) -> void
     }
 }
 
+auto Sarrus_method(int matrix[20][20]) -> int
+{
+    int oper1 = matrix[0][0] * matrix[1][1] * matrix[2][2] + matrix[0][1] * matrix[1][2] * matrix[2][0] + matrix[0][2] * matrix[1][0] * matrix[2][1];
+    int oper2 = matrix[0][2] * matrix[1][1] * matrix[2][1] + matrix[0][0] * matrix[1][2] * matrix[2][1] + matrix[0][1] * matrix[1][0] * matrix[2][2];
+    int result = oper1 - oper2;
+
+    return result;
+}
+
+// auto Laplace_method (int matrix, int size) -> int
+// {
+//     int * tempmatrix = new int[size * size];
+//     int oper;
+//     int det=0;
+
+//     for (auto i = 0; i < size; i++) 
+//     {
+//         for(auto j = 0; j < size; j++)
+//         {
+//             oper = (i)*size+(j+1);
+//             tempmatrix[i*size+j] = matrix[oper];
+//         }
+//     }
+
+//     display_matrix(tempmatrix, size-1, size-1);
+
+//     return det;
+// }
+
 auto main() -> int
 {
     int calc;
+
+    int matrixA[20][20];
+    int row1, col1;
+    
+    int matrixB[20][20];
+    int row2, col2;
 
     std::cout << "Kalkulator macierzowy\n";
     std::cout << "Proszę wybrać odpowiednią opcję:\n";
@@ -85,39 +136,36 @@ auto main() -> int
     switch(calc)
     {
         case 1:
-        {
-            int row1, col1;
-            
+        {             
             std::cout << "Podaj ilosc wierszy pierwszej macierzy: "; 
             std::cin >> row1;
             std::cout << "Podaj ilość kolumn pierwszej macierzy: ";
             std::cin >> col1;
 
             std::cout << "Wprowadź elementy do pierwszej macierzy: \n";
-            int * matrixA = enter_matrix(row1, col1);
-
-
-            int row2, col2;
+            matrixA = enter_matrix(row1, col1);
+            
 
             std::cout << "Podaj ilosc wierszy drugiej macierzy: "; 
             std::cin >> row2;
             std::cout << "Podaj ilość kolumn drugiej macierzy: ";
             std::cin >> col2;
             
-            std::cout << "Wprowadź elementy do drugiej macierzy: \n";
-            int * matrixB = enter_matrix(row2, col2);
 
-            
             //jeśli macierze mają inne rozmiary to zakończ program
-            if(row1!=row2 || col2!=col2)
+            if(row1!=row2 || col1!=col2)
             {   
                 atexit(error);
                 exit(0);
-            }       
+            }
 
 
-            int* add =  new int [row1 * col1];
-            int* sub = new int [row1 * col1];
+            std::cout << "Wprowadź elementy do drugiej macierzy: \n";
+            matrixB = enter_matrix(row2, col2);        
+
+
+            int add[20][20];
+            int sub[20][20];
 
 
             //dodawanie i odejmowanie macierz
@@ -125,8 +173,8 @@ auto main() -> int
             {
                 for(auto j = 0; j < col1; j++)
                 {
-                    add[i*row1+j] = matrixA[i*row1+j] + matrixB[i*col1+j];
-                    sub[i*row1+j] = matrixA[i*row1+j] - matrixB[i*col1+j];
+                    add[i][j] = matrixA[i][j] + matrixB[i][j];
+                    sub[i][j] = matrixA[i][j] - matrixB[i][j];
                 }
             }
 
@@ -136,7 +184,7 @@ auto main() -> int
             display_matrix(add, row1, col2);
 
 
-            //wyświetlenie resultu odejmowania
+            //wyświetlenie wyniku odejmowania
             std::cout << "Wynik odejmowania:\n";
             display_matrix(sub, row1, col2);
 
@@ -145,7 +193,7 @@ auto main() -> int
         }
         case 2:
         {
-            int row1, col1;
+            
             
             std::cout << "Podaj ilość wierszy pierwszej macierzy: "; 
             std::cin >> row1;
@@ -153,19 +201,14 @@ auto main() -> int
             std::cin >> col1;
 
             std::cout << "Wprowadź elementy do pierwszej macierzy: \n";
-            int * matrixA = enter_matrix(row1, col1);
+            matrixA = enter_matrix(row1, col1);
 
-
-            int row2, col2;
 
             std::cout << "Podaj ilość wierszy drugiej macierzy: "; 
             std::cin >> row2;
             std::cout << "Podaj ilość kolumn drugiej macierzy: ";
             std::cin >> col2;
 
-            std::cout << "Wprowadź elementy do drugiej macierzy: \n";
-            int * matrixB = enter_matrix(row2, col2);
-            
 
             //jeśli liczba kolumn w macierzy A i liczba wierszy w macierzy B są różne to zakończ program
             if(col1!=row2)
@@ -175,7 +218,11 @@ auto main() -> int
             }
 
 
-            int* result =  new int [col1 * row2];
+            std::cout << "Wprowadź elementy do drugiej macierzy: \n";
+            matrixB = enter_matrix(row2, col2);
+
+
+            int result[20][20];
             int temp;
             
             for(auto i = 0; i < row1; i++)
@@ -185,9 +232,9 @@ auto main() -> int
                     temp = 0;
                     for(auto k = 0; k < row1; k++)
                     {
-                        temp += matrixA[i*col1+k] * matrixB[k*col2+j];
+                        temp += matrixA[i][k] * matrixB[k][j];
                     }                 
-                    result[i*col2+j] = temp;
+                    result[i][j] = temp;
                 }
             }
 
@@ -197,7 +244,7 @@ auto main() -> int
             {
                 for(auto j = 0; j < col2; j++)
                 {
-                    std::cout << result[i*col2+j];
+                    std::cout << result[i][j];
                     if(j==col2-1)
                     {
                         std::cout << "\n";
@@ -213,7 +260,7 @@ auto main() -> int
         }
         case 3:
         {   
-            int row1, col1;
+            
             
             std::cout << "Podaj ilość wierszy macierzy: "; 
             std::cin >> row1;
@@ -221,7 +268,7 @@ auto main() -> int
             std::cin >> col1;
 
             std::cout << "Wprowadź elementy do macierzy: \n";
-            int* matrixA = enter_matrix(row1, col1);
+            matrixA = enter_matrix(row1, col1);
             
 
             //wyświetlenie podanej przez uzytkownika macierzy
@@ -232,12 +279,12 @@ auto main() -> int
 
 
             //wyświetlenie transponowanej macierzy
-            std::cout << "Macierz transponowana: \n";
+            std::cout << "\nMacierz transponowana: \n";
             for(auto i = 0; i < row1; i++)
             {
                 for(auto j = 0; j < col1; j++)
                 {
-                    std::cout << matrixA[j*row1+i];
+                    std::cout << matrixA[j][i];
                     if(j==col1-1)
                     {
                         std::cout << "\n";
@@ -254,8 +301,8 @@ auto main() -> int
         }
         case 4:
         {
-            int row1, col1;
-            
+            int result1;
+
             std::cout << "Podaj ilość wierszy macierzy: "; 
             std::cin >> row1;
             std::cout << "Podaj ilość kolumn macierzy: ";
@@ -271,10 +318,31 @@ auto main() -> int
 
 
             std::cout << "Wprowadź elementy do macierzy: \n";
-            int* matrixA = enter_matrix(row1, col1);
+            matrixA = enter_matrix(row1, col1);
 
-            
+            if(row1 == 1 && col1 == 1)
+            {
+                result1 = matrixA[0][0]; 
+            }
+            else if(row1 == 2 && col1 == 2)
+            {
+                result1 = matrixA[0][0] * matrixA[1][1] - matrixA[0][1] * matrixA[1][1];
+            }
+            else if(row1 == 3 && col1 == 3)
+            {
+                //Metoda Sarrusa;
+                result1 = Sarrus_method(matrixA);
+            }
+            else
+            {
+                //Metoda Laplace'a
+                //result1 = Laplace_method(matrixA, row1); //<- potrzebny jest tylko jeden rozmiar jesli obydwie liczby są takie same 
+            }
 
+            display_matrix(matrixA, row1, col1);
+
+
+            std::cout << "\nWyznacznik macierzy: " << result1 << "\n";
 
 
             break;
