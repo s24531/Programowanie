3d9c3629d87645dd8c84e1e840765aaa29808506
Student <s24531@pjwstk.edu.pl>
2022-01-14 12:58:41 +0100

    Zadanie

--- Zadania_nowe2/Zadanie_7.cpp
+++ Zadania_nowe2/Zadanie_7.cpp
@@ -4,40 +4,31 @@
 
 struct group
 {
-  
   std::string nazwa;
   std::vector<std::string> studenci;
 
-    
+  group(std::string a, std::vector<std::string> s)
+  {
+    nazwa = a;
+    studenci = s;
+  }
+   
 };
 
-auto enter_into_group(std::vector<group> stud, std::string student) -> void
+auto enter_into_group(std::vector<std::string> stud, std::string uczen) -> void
 {
-    stud.push_back(student);
-
+  stud.push_back(uczen);
 
 }
 
-
-int main()
+auto main() -> int
 {
-    std::string imie;
-    std::vector<group> stud;
+  std::vector<std::string> p;
 
-    std::cout << "Podaj imie studenta: ";
-    std::cin >> imie;
+  group st1("Bartek", p);
 
-    group st1("Maciek");
-    group st2("Bartek");
-    group st3("Paweł");
-    group st4("Jan");
+  enter_into_group(p, st1.nazwa);
 
-    stud.push_back(st1);
-    stud.push_back(st2);
-    stud.push_back(st3);
-    stud.push_back(st4);
-
-    enter_into_group(stud, imie);
 
     return 0;
 }
