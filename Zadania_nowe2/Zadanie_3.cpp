5d1dce9fc7666c0c1930bef79f4b4fcc87c30ed9
Student <s24531@pjwstk.edu.pl>
2022-01-14 14:04:50 +0100

    Zadanie

--- Zadania_nowe2/Zadanie_3.cpp
+++ Zadania_nowe2/Zadanie_3.cpp
@@ -3,8 +3,14 @@
 
 auto dot_product(std::vector<double> wektor, std::vector<double> wektor2) -> double
 {
-    double wynik = (wektor[0] * wektor2[0]) + (wektor[1] * wektor2[1]);
-    
+    double wynik = 0;
+
+    for(auto i = 0; i < wektor.size(); i++)
+    {
+        wynik = wynik + (wektor[i] * wektor2[i]); 
+    }
+
+
     return wynik;
 }
 
@@ -15,20 +21,20 @@ auto main() -> int
 
     double liczba, liczba2;
 
-    for(auto i=0;i<2;i++)
+    int wiel = 0;
+    std::cout << "Podaj wielkosć wektora: ";
+    std::cin >> wiel;
+
+    for(auto i=0;i<wiel;i++)
     {
         std::cout << "Podaj wspolrzedne 1 wektora: ";
         std::cin>>liczba;
         wektor.push_back(liczba);
-    }
 
-    std::cout << "\n";
-    
-    for(auto i=0;i<2;i++)
-    {
         std::cout << "Podaj wspolrzedne 2 wektora: ";
         std::cin>>liczba2;
         wektor2.push_back(liczba2);
+        std::cout<<std::endl;
     }
 
     std::cout << "Iloczyn skalarny wynosi: " << dot_product(wektor, wektor2) << "\n";
