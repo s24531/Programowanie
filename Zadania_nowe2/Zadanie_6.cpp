a0a98a25bd1725d6c6b3e466d6e09765c9eb5101
s24531 <s24531@pjwstk.edu.pl>
2022-01-27 15:08:21 +0100

    Zadanie

--- Zadania_nowe2/Zadanie_6.cpp
+++ Zadania_nowe2/Zadanie_6.cpp
@@ -4,65 +4,54 @@
 
 struct student
 {
-  
   std::string imie;
-  float srednia;
   std::vector<int> oc;
-  
-    student(std::string i, std::vector<int> oc)
-    {
-        imie = i;
-        int suma = 0;
-
-        for(auto i = 0; i < oc.size(); i++)
-        {
-            suma+=oc[i];
-        }
-
-        srednia = suma/oc.size();
-    }
     
 };
 
-auto get_best_student(std::vector<student> uczen) -> void
+auto average_of(student Student) -> double
+{   
+    double wynik=0; 
+    double srednia;
+
+    for(auto i=0; i<=Student.oc.size(); i++)
+    {
+        wynik+=Student.oc[i];
+    }
+    
+    srednia=wynik/Student.oc.size();
+
+    return srednia;
+}
+
+auto get_best_student(std::vector<student> uczen) -> student
 {
     float bestmark = 0;    
-    std::string bestname;
+    auto bestname = student{};
 
-    for(auto i = 0; i < uczen.size(); i++)
+    for(auto a : uczen)
     {
-        if(uczen[i].srednia > bestmark)
+        if(average_of(a) > bestmark)
         {
-            bestmark =  uczen[i].srednia;
-            bestname = uczen[i].imie;
+            bestmark = average_of(a);
+            bestname = a;
         }
     }
     
-    std::cout << "Imię: " << bestname << "\n";
-    std::cout << "Średnia: " << bestmark << "\n";
+    return bestname;
     
 }
 
 int main()
 {
-    std::vector<student> stud;
+    auto st1 = student {"Maciek",{3,5,4,1}};
+    auto st2 = student {"Bartek",{1,4,2,5}};
+    auto st3 = student {"Paweł",{2,2,6,3}};
+    auto st4 = student {"Jan",{4,6,4,2}};
 
-    std::vector<int> mark1 = {3,5,4,1};
-    std::vector<int> mark2 = {1,4,2,5};
-    std::vector<int> mark3 = {2,2,6,3};
-    std::vector<int> mark4 = {4,6,4,2};
-
-    student st1("Maciek",mark1);
-    student st2("Bartek",mark2);
-    student st3("Paweł",mark3);
-    student st4("Jan",mark4);
-
-    stud.push_back(st1);
-    stud.push_back(st2);
-    stud.push_back(st3);
-    stud.push_back(st4);
+    auto stud = std::vector<student> {st1,st2,st3,st4};
        
-    get_best_student(stud);
+    std::cout << get_best_student(stud).imie << "\n";
 
 
     return 0;
